b7cab570e2d62100ddd2db5eaa7264fac23cfc1a
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-01-28 17:58:57 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+// accountId = 2; //not allowed
+console.log(accountId);
+
+accountEmail = "hc@hc.com";
+accountPassword = "2121";
+accountCity = "banglore";
+//hi
+
+/* Prefer not to use var because of issue in block scope and functional block */
+
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+
+
