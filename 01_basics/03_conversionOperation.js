a8ac44ee8fbc86c48069cf80f12c8b2f7b06deaa
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-01-28 18:52:15 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "33aa";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" -> 33 can be easily converted to 33
+// "33abc" -> NaN (not a number)
+// "true" -> 1
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
