0778af187d4344b0ecab724b94b0bab111da3c11
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-01-28 22:29:45 +0530

    convertion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,6 +18,33 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// operations -
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 raise to power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1+str2;
+console.log(str3);
+
+
+console.log("1" + 2);    //12
+console.log("1" + 2 + 2); //123
+console.log(1 + "2"); //12
+console.log(1 + 2 + "2"); //32 , because here number type is first
+
+console.log(+true);
+console.log(+"");
+
 
 
 
