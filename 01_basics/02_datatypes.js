497a89ac6868a1ab1098d6eb5ca952c016c2745f
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-01-28 18:36:17 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); this will not work here since we are using nodejs and not browser    
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// types of datatypes in js - 
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+console.log(typeof("hi"));
+
+
+
+
+
+
