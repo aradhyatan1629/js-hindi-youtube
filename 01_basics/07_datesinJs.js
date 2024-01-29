4c40ee6bf781f3db6b8429b04a65c22373e316eb
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-01-29 15:26:31 +0530

    dates in js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,31 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+
+// let myCreatedDate = new Date(2024,0,29);
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("2024-01-29");
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
