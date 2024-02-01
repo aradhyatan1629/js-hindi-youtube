fefb95621a5b9b4217d556e09ac8ff74c56df23e
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-01 23:11:19 +0530

    function 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,3 +44,42 @@ console.log(loginUserMessage("hitesh"));
 console.log(loginUserMessage());
 
 
+// function with objects and array in javascript - 
+
+function calculateCartPrice(val1,val2,...num1)  // rest operator
+{
+    return num1;
+}
+console.log(calculateCartPrice(200,400,500,2000));
+
+const user = 
+{
+    userName : "hitesh",
+    price : 199
+};
+
+function handleObject(anyobj)
+{
+    console.log(`Username is ${anyobj.userName} and price is ${anyobj.price}`);
+}
+// handleObject(user);
+handleObject(
+    {
+        username:"hitesh",
+        price:199
+    }
+);
+
+const myNewArray = [200,400,100,600];
+function returnSecondValue(getArray)
+{
+    return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
+
+
+
+
+
+
