2abd7f3c45e5d4818c055907fc29e98d11c7b8d9
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-02 15:24:58 +0530

    switch stmt

---
+++ 04_contol_flow/switch.js
@@ -0,0 +1,37 @@
+// switch in js - 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2;
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default:
+        console.log("enter number b/w 1 and 12");
+}
+
+
+
+
+
+
+
+
+
+
+
+
