25627a3ecbf5fe322b9b692fd53d3bcce0f8681d
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-02 15:43:43 +0530

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,24 @@
+// for loop - 
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+
+let myArr = ["flash","batman","superman"];
+for(let i=0;i<myArr.length;i++)
+{
+    console.log(myArr[i]);
+}
+
+// break and continue 
+
+
+
+
+
+
+
+
+
+
+
