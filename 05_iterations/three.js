f65c841c05f38e45fe31614a5299cabcef644e2c
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-03 15:36:12 +0530

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,44 @@
+// for of loop - 
+
+const arr = [1,2,3,4,5];
+for (const num of arr) 
+{
+    console.log(num);
+}
+
+const greetings = "Hello world";
+for(const greet of greetings)
+{
+    console.log(greet);
+}
+
+//maps - 
+const map = new Map();
+map.set("In","India");
+map.set("USA","United state of america");   
+map.set("Fr","france");
+
+// console.log(map);
+
+for(const [key,value] of map)
+{
+    console.log(key, " ",value);
+}
+
+const myObj = 
+{
+    "game1" : "nfs",
+    "game2" : "spiderman"
+};
+
+// for (const [key,value] of myObj) {
+//     console.log(key, " ",value);
+// }
+
+
+
+
+
+
+
+
