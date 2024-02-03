6547e737db5c7013fbe91a6b5b8921ad4780b294
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-03 15:59:28 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+const myObj = 
+{
+    js : "javadcript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+};
+
+for (const key in myObj) {
+    console.log(`${key} : ${myObj[key]}`);
+}
+
+const programming = ["js","rb","py","java","cpp"];
+for(const key in programming)
+{
+    console.log(programming[key]);
+}
+
+
+
+
+
+
+
