14492809b0cd22ae7504a71c88e11194b318c596
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-02-20 02:01:29 +0530

    for each loop and array of objects

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -15,9 +15,26 @@ coding.forEach((item,index,arr)=>{
     console.log(item,index,arr);
 })
 
+// this is an array of objects
+const codeing = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    },
+]
 
-
-
+// we can iterate over the array of objects as follows - 
+codeing.forEach((item)=>{
+    console.log(item.languageName);
+})
 
 
 
