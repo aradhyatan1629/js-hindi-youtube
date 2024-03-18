31d6fa65352b457f8a6d5e6e65456fae3947fd79
aradhyatan1629 <aradhyataneja44@gmail.com>
2024-03-18 14:27:22 +0530

    more on objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,12 +24,32 @@ console.log(regularUser.fullname.userfullname.firstname);
 const obj1 = {1:"a",2:"b"};
 const obj2 = {3:"a",4:"b"};
 
-// const obj3 = {pbj1,obj2};
+// const obj3 = {obj1,obj2};
 // const obj3 = Object.assign({},obj1,obj2);
 
 const obj3 = {...obj1,...obj2};
 console.log(obj3);
 
 
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },  
+    {
+        id:1,
+        email:"h@gmail.com"
+    },    
+]
+
+// console.log(users[1].email);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
